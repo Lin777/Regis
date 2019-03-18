65d70d25d8d036efb00ee4e6173878dcd8034b1a
Evelyn Cusi Lopez <cusi.evelyn@gmail.com>
2019-03-18 09:59:40 -0400

    Adding files to deploy with Docker

---
+++ Docker/esugDocker.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+screen -S esug -X quit
+
+# clean the place
+rm -rf {esug.*,pharo*,*.sources,Pharo*}
+
+wget -O- https://get.pharo.org | bash
+
+./pharo Pharo.image save esug
+
+./pharo esug.image metacello install github://Lin777/ESUGConfRegistrationApp:master/src BaselineOfESUGApp --install=stable
+
+./pharo esug.image eval --save "
+| application |
+ERSandstoneDatabase initialize.
+ESUGSetUp start.
+ERTest generateDataForTest.
+application := WAAdmin defaultDispatcher handlerAt: 'config'.
+application configuration addParent: WAAuthConfiguration instance.
+application
+        preferenceAt: #login put: 'admin';
+        preferenceAt: #passwordHash put: (GRPlatform current secureHashFor: 'SomePassword').
+application addFilter: WAAuthenticationFilter new.
+
+WAAdmin disableDevelopmentTools.
+
+WAAdmin defaultDispatcher defaultName: 'ESUG'.
+"
+
+# Launch the server in a new screen.
+#screen -Sdm esug ./pharo esug.image --no-quit
